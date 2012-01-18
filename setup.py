60f16aaba8cf18b442992160a974455742e8dad6
Rijk Stofberg <rijk.stofberg@gmail.com>
2012-01-18 23:04:46 +0200

    Add test extra

--- setup.py
+++ setup.py
@@ -30,6 +30,11 @@ setup(name='siyavula.what',
           'plone.namedfile [blobs]',
           # -*- Extra requirements: -*-
       ],
+      extras_require={
+        'test': [
+          'plone.app.testing',
+        ],
+      },
       entry_points="""
       # -*- Entry points: -*-
       [z3c.autoinclude.plugin]
