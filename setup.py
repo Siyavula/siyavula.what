eccb1eed6b5c50d412234eca75e78986470692c0
Rijk Stofberg <rijk.stofberg@gmail.com>
2012-01-19 21:21:32 +0200

    Name change

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ version = '0.1'
 
 setup(name='siyavula.what',
       version=version,
-      description="Siyavula What? Questions and Answers on Plone content.",
+      description="siyavula.what",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
