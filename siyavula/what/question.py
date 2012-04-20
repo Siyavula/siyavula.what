1d4abc5bd2f28a71538615e656de86e877506249
Rijk Stofberg <rijk@upfrontsystems.co.za>
2012-04-20 13:59:13 +0200

    Restrict answers to answer items... not everything in the folder.

--- siyavula/what/question.py
+++ siyavula/what/question.py
@@ -60,4 +60,5 @@ class Question(dexterity.Container):
 
     @property
     def answers(self):
-        return self.objectValues()
+        obs = self.objectValues()
+        return [ob for ob in obs if ob.portal_type == 'siyavula.what.answer']
