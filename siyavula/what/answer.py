d449015b9613f05b0df9d5da69021b31c210bbe6
Rijk Stofberg <rijk@upfrontsystems.co.za>
2012-04-20 13:58:37 +0200

    Make answer a richtext field. Remove title override and do a little import cleanup.

--- siyavula/what/answer.py
+++ siyavula/what/answer.py
@@ -1,22 +1,8 @@
 from five import grok
 from plone.directives import dexterity, form
 
-from zope import schema
-from zope.schema.interfaces import IContextSourceBinder
-from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
-
-from zope.interface import invariant, Invalid
-
-from z3c.form import group, field
-
 from plone.namedfile.interfaces import IImageScaleTraversable
-from plone.namedfile.field import NamedImage, NamedFile
-from plone.namedfile.field import NamedBlobImage, NamedBlobFile
-
 from plone.app.textfield import RichText
-
-from z3c.relationfield.schema import RelationList, RelationChoice
-from plone.formwidget.contenttree import ObjPathSourceBinder
 
 from siyavula.what import MessageFactory as _
 
@@ -26,7 +12,7 @@ class IAnswer(form.Schema, IImageScaleTraversable):
     An answer.
     """
 
-    text = schema.Text(
+    text = RichText(
         title=_(u"Answer"),
         description=_("The answer."),
         required=True,
@@ -35,9 +21,3 @@ class IAnswer(form.Schema, IImageScaleTraversable):
 
 class Answer(dexterity.Item):
     grok.implements(IAnswer)
-
-    def Title(self):
-        return self.text
-
-    def setTitle(self, value):
-        pass
